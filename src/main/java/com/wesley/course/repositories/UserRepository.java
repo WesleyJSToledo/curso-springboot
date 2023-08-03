c99d748dd44fae8879fb28d3dc360cbf1ff50bdf
WesleyJSToledo <wesleyjstoledo@gmail.com>
2023-08-03 10:48:42 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/wesley/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.wesley.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wesley.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
