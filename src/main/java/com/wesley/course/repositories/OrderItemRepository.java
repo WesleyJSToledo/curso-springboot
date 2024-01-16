5a39b3de48a1dba9362c684567900246ece8d48d
WesleyJSToledo <wesleyjstoledo@gmail.com>
2024-01-16 16:09:24 -0300

    adjustment

--- src/main/java/com/wesley/course/repositories/OrderItemRepository.java
+++ src/main/java/com/wesley/course/repositories/OrderItemRepository.java
@@ -3,7 +3,6 @@ package com.wesley.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wesley.course.entities.OrderItem;
-import com.wesley.course.entities.pk.OrderItemPK;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
 
